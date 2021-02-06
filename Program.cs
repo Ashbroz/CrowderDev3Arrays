836c2fbc0ef23f60ba6737d2debcc797291bbb95
Ashton Crowder <acrowder2@usf.edu>
2021-02-05 20:19:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Author: Ashton Crowder
+// Date: 2/5/2021
+
+using System;
+
+namespace CrowderDev3Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Populating Array of 25
+            int[] arr = new int[25];
+            // Count Starting from 1
+            int j = 1;
+            // Count from 1 to 25
+            for (int i = 0; i < 25; i++)
+            {
+                arr[i] = j;
+                j++;
+            }
+            
+            for (int i = 0; i < 25; i++)
+            {
+                // display array using writeline
+                Console.WriteLine("Element Value = ");
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
